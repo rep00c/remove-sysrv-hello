9716378a5654d5516b24480350ee6bb8781c134e
Dig2 <55265626+WgagaXnunigo@users.noreply.github.com>
2021-04-13 11:07:46 +0800

    Update remove-sysrv-hello.sh

--- remove-sysrv-hello.sh
+++ remove-sysrv-hello.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+
+getTop1(){
+    top1=$(ps -aux | sort -k3nr | head -n 1 | awk '{print $11 " " $2 " " $3}')
+}
+
 echo 'Searching process takes most CPU...'
 
-top1=$(ps -aux | sort -k3nr | head -n 1)
-top1_much=$(echo ${top1} | awk '{print $3}')
-top1_pid=$(echo ${top1} | awk '{print $2}')
-top1_name=$(echo ${top1} | awk '{print $11}')
-echo "Status: ${top1_name} ${top1_pid} ${top1_much}"
-
-if [ $(echo "${top1_much}>75" | bc) = 1 ] && [ $(echo ${top1_name}) = "[kthreaddi]" ]
+getTop1
+echo $top1 | awk '{print "Process:" $1 " PID:" $2 " CPU:%" $3}'
+if [ $(echo ${top1} | awk '{print $3">1" }'| bc) = 1 ] && [ $(echo ${top1} | awk '{print $1}') = "[kthreaddi]" ]
 then
     echo "Sysrv-hello exists"
 else
@@ -16,7 +17,7 @@ else
 fi
 
 echo -n "killing..."
-kill $(echo ${top1_pid})
+kill $(echo ${top1} | awk '{print $2}')
 if [ "$?" -ne 0 ]
 then
     echo "command failed"
